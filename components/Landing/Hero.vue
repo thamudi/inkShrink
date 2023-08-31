c8b567ec93b850770e0a5a0eaf8dfe8b7c4a8308
Tamim Hamoudi <tamim.hamoudi@gmail.com>
2023-08-31 17:58:20 +0300

    feat: add hero banner

---
+++ components/Landing/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container hero-container">
+    <h1>Artificial intelligence can write, but can it influence?</h1>
+    <NuxtImg
+      src="/logo/hero.svg"
+      alt="hero banner"
+      width="550"
+      height="250"
+      class=""
+    />
+  </div>
+</template>
+<style scoped lang="postcss">
+  .hero-container {
+    @apply flex flex-col gap-y-20;
+    @apply gap-x-20 lg:flex-row lg:items-end;
+  }
+</style>
