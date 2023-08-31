e84c2ad731bad4dede59badbdfe10795d2c22857
Tamim Hamoudi <tamim.hamoudi@gmail.com>
2023-08-31 17:37:04 +0300

    feat: add app nav bar and header

---
+++ components/Menu/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="nav-container">
+    <MenuItems />
+  </nav>
+</template>
+<script setup></script>
+<style scoped lang="postcss">
+  .nav-container {
+    @apply w-full justify-end gap-x-5;
+    @apply border-b-2 lg:border-none;
+  }
+</style>
---
+++ components/Ui/MenuIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="block lg:hidden cursor-pointer">
+    <div v-if="!props.isOpen">
+      <NuxtImg
+        src="/icons/burger.svg"
+        alt="navigation icon"
+        width="30"
+        height="30"
+        @click="$emit('toggle-menu')"
+      />
+    </div>
+    <div v-else>
+      <NuxtImg
+        src="/icons/close.svg"
+        alt="navigation icon"
+        width="30"
+        height="30"
+        @click="$emit('toggle-menu')"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+  defineEmits(['toggle-menu'])
+  const props = defineProps({
+    isOpen: {
+      type: Boolean,
+      required: true,
+    },
+  })
+</script>
