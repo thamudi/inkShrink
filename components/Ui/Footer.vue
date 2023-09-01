e0a7f788db5f2ba33ac3c5e22cd2d49590aa00ce
Tamim Hamoudi <tamim.hamoudi@gmail.com>
2023-09-01 10:39:19 +0300

    fix: quick footer alignement fix

--- components/Ui/Footer.vue
+++ components/Ui/Footer.vue
@@ -2,9 +2,7 @@
   <footer
     class="container border-2 border-b-0 flex flex-col lg:flex-row justify-between items-center w-full"
   >
-    <div
-      class="w-full block gap-y-5 items-center gap-x-5 py-10 lg:flex lg:justify-between"
-    >
+    <div class="w-full block gap-y-5 items-center gap-x-5 py-10 lg:flex">
       <NuxtLink to="/">
         <NuxtImg src="/logo/logo-white.svg" width="278" height="83" />
       </NuxtLink>
