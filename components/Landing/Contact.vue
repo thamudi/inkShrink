07ba6593d7145480a91e521af1a20ad4e5d9fd6a
Tamim Hamoudi <tamim.hamoudi@gmail.com>
2023-09-01 07:56:26 +0300

    feat: finalize contact us form

--- components/Landing/Contact.vue
+++ components/Landing/Contact.vue
@@ -3,29 +3,93 @@
     <h1>Get in touch</h1>
 
     <div class="my-20">
-      <form class="flex flex-col gap-y-5" v-on:submit.prevent="">
+      <form
+        class="flex flex-col gap-y-5"
+        @submit.prevent="submitForm"
+        id="contactForm"
+      >
         <div class="flex flex-col gap-y-5 lg:flex-row lg:gap-x-5">
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Topic/Company" />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            v-model="form.name"
+          />
+          <input
+            type="text"
+            placeholder="Topic/Company"
+            name="topic"
+            v-model="form.topic"
+          />
         </div>
-        <input type="email" placeholder="Email" />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          v-model="form.email"
+        />
         <textarea
           class="h-52"
           name="message"
           id="message"
+          v-model="form.message"
           placeholder="Message"
         ></textarea>
 
-        <button class="" type="submit">Submit</button>
+        <button :disabled="disabled" type="submit">Submit</button>
       </form>
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+  import { get, set } from '@vueuse/core'
+  import { ElNotification } from 'element-plus'
+
+  const disabled = ref(false)
+  const submitted = ref(false)
+  const form = reactive({
+    name: '',
+    topic: '',
+    email: '',
+    message: '',
+  })
+
+  const submitForm = async () => {
+    set(disabled, true)
+    const { name, topic, email, message } = toRefs(form)
+
+    try {
+      const contactResponse = await $fetch('/api/contact', {
+        method: 'POST',
+        body: JSON.stringify({
+          name: get(name),
+          topic: get(topic),
+          email: get(email),
+          message: get(message),
+        }),
+      })
+      buildToast('success', get(contactResponse)?.message)
+    } catch (error) {
+      buildToast('error', get(error)?.data?.message)
+    } finally {
+      set(disabled, false)
+    }
+  }
+
+  function buildToast(type, message) {
+    ElNotification({
+      title: type === 'success' ? 'Success' : 'Failed',
+      message: message,
+      type: type,
+      duration: 5000,
+      position: 'bottom-right',
+    })
+  }
+</script>
 <style scoped lang="postcss">
-  input,
-  textarea,
-  button {
+  #contactForm input,
+  #contactForm textarea,
+  #contactForm button {
     @apply rounded-lg;
     @apply text-lg;
     @apply border-2 border-white;
@@ -34,11 +98,14 @@
     @apply w-full;
   }
 
-  button {
+  #contactForm button {
     @apply text-inkshrink-black;
     @apply w-60;
     @apply font-bold;
-    /* @apply px-14 py-4; */
     @apply bg-inkshrink-green;
+  }
+
+  #contactForm button:disabled {
+    @apply bg-inkshrink-black text-gray-500;
   }
 </style>
