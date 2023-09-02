f340a714f133dddf7250b7e086702a0da3582d54
Tamim Hamoudi <tamim.hamoudi@gmail.com>
2023-09-02 11:58:43 +0300

    feat: add nuxt seo to homepage

--- app.vue
+++ app.vue
@@ -7,3 +7,16 @@
   </div>
   <UiFooter />
 </template>
+<script setup>
+  useSeoMeta({
+    title: () => `InkShrink`,
+    ogTitle: () => `InkShrink`,
+    twitterTitle: () => `InkShrink`,
+    description: () =>
+      `Ink Shrink is a content writing service that specializes in humanizing content in an AI era. Ink Shrink provides content strategies, quality writing, and proofreading for individuals and small businesses. Ink shrink transforms rigid long proposals, articles, blogs, and other documents into an understandable, concise, and relished documents.`,
+    ogDescription: () =>
+      `Ink Shrink is a content writing service that specializes in humanizing content in an AI era. Ink Shrink provides content strategies, quality writing, and proofreading for individuals and small businesses. Ink shrink transforms rigid long proposals, articles, blogs, and other documents into an understandable, concise, and relished documents.`,
+    twitterDescription: () =>
+      `Ink shrink transforms rigid long proposals, articles, blogs, and other documents into an understandable, concise, and relished documents.`,
+  })
+</script>
